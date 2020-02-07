c3194e2970d41f8405c53997d9361b2e9543809d
sinamoraddar <sinamoraddar@gmail.com>
2020-02-07 10:49:16 +0330

    Updates

---
+++ component---src-pages-404-js-1cfd05bf0d362725f3ce.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,n,a){"use strict";a.r(n);var t=a("q1tI"),l=a.n(t),r=a("7oih"),c=a("Wbzz");n.default=function(){return l.a.createElement(r.a,{fullMenu:!0},l.a.createElement("section",{id:"wrapper"},l.a.createElement("header",null,l.a.createElement("div",{className:"inner"},l.a.createElement("h2",null,"صفحه مورد نظر پیدا نشد"),l.a.createElement(c.Link,{to:"/"},"صفحه اصلی")))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-1cfd05bf0d362725f3ce.js.map
